9c81904ab0cfad831d19af3d278c4c467abd0455
Odevsa <dev.raphaelsa@gmail.com>
2026-09-13 01:24:37 -0300

    fix: Improve default config

--- src/config.rs
+++ src/config.rs
@@ -30,7 +30,7 @@ impl Default for Config {
             light_color: [0xCC, 0xCC, 0xCC],
             shadow_color: [0x00, 0x00, 0x00],
             object_scale: 1.0,
-            camera_eye: [1.5, 1.5, 1.5],
+            camera_eye: [-1.0, -1.0, 1.0],
             scroll_speed: 0.01,
             invert_scroll: false,
             scroll_min: 0.5,
